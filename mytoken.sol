cfe10256dfaaef2bdfb69520e81f1ef2e23d5115
ketansaini <86094856+iketansaini@users.noreply.github.com>
2023-06-20 19:22:54 +0530

    Add files via upload

---
+++ mytoken.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+contract MyToken {
+    // Public variables that store the details about the token
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint256 public totalSupply = 0;
+    // Mapping of addresses to balances
+    mapping(address => uint256) public balances;
+    // Mint function
+    function mint(address to, uint256 amount) public {
+        
+        totalSupply += amount;
+        balances[to] += amount;
+    }
+    // Burn function
+    function burn(address from, uint256 amount) public {
+        require(balances[from] >= amount, "Not enough tokens to burn");
+        totalSupply -= amount;
+        balances[from] -= amount;
+    }
+}
